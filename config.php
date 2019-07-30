c685dac26e79b66e3d7b84728ac78ba698ba335e
Worm Toxic <52546790+wormtoxic@users.noreply.github.com>
2019-07-30 13:50:33 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+//Enter PHPSESSID
+$PHPSESSID = 'nefhuiwehgjksdngjks';
+//Enter GA
+$_ga = 'GA1.2.2106547567267.15625688560730';
+//Enter GID
+$_gid = 'GA1.2.567568568.15645475785685016';
+
+
+
+?>
